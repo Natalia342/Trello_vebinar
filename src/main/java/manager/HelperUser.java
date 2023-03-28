a78a212033c79b91433e7dc208162acdae61f524
Natalia342 <nataliatalova7@gmail.com>
2023-03-28 09:16:45 +0300

    Assert

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,18 +1,34 @@
 package manager;
 
 import models.User;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+
+
+    public void initLogin(){
+        click(By.cssSelector("a[href='/login'"));
+    }
 public void fillLogin(User user){//napolnit
 String login = user.getEmail();
+type(By.id("user"), login);
+
 
 }
+public void clickLogin(){
+    click(By.id("login"));
+}
 public void fillPassword(User user){
-        String password= user.getPassword();
+        String password= user.getPassword();//
+        type(By.id("password"),password);
+}
+public void submitPassword(){
+        click(By.id("login-submit"));
 }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
 import models.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
@@ -8,6 +10,13 @@ public class LoginTests extends TestBase{
                 .email("nataliatalova7@gmail.com")
                 .password("1234567$Yy")
                 .build();
-
+        app.getUser().initLogin();
+        app.getUser().fillLogin(user);
+        app.getUser().clickLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillPassword(user);
+        app.getUser().submitPassword();
+        app.getUser().pause(2000);
+//        Assert.assertTrue(app.getUser().isElementPresent(By.className(".DweEFaF5owOe02 rQ86P0iNikD4I9 Cg0RMJhBknTRbM")));
     }
 }
